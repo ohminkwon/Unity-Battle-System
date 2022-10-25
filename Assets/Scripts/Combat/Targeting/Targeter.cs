492a295c0d2fcbf800de6d75149e8a5f34a99499
ohminkwon <ohminkwon0720@gmail.com>
2022-10-25 23:36:51 +0900

    20-Edit functions for Cinemachine Target Group

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Targeter : MonoBehaviour
 {
     [SerializeField] private List<Target> targets = new List<Target>();
+    [SerializeField] private CinemachineTargetGroup cinemachineTargetGroup;
 
     public Target CurrentTarget { get; private set; }
 
@@ -14,13 +16,14 @@ public class Targeter : MonoBehaviour
             return;
 
         targets.Add(target);
+        target.OnDestroyEvent += RemoveTarget;
     }
     private void OnTriggerExit(Collider other)
     {
         if (!other.TryGetComponent<Target>(out Target target))
             return;
 
-        targets.Remove(target);
+        RemoveTarget(target);
     }
 
     public bool SelectTarget()
@@ -29,11 +32,28 @@ public class Targeter : MonoBehaviour
             return false;
 
         CurrentTarget = targets[0];
+        cinemachineTargetGroup.AddMember(CurrentTarget.transform, 1f, 2f);
         return true;
     }
     public void Cancel()
     {
+        if (CurrentTarget == null)
+            return;
+
+        cinemachineTargetGroup.RemoveMember(CurrentTarget.transform);
         CurrentTarget = null;
+    }
+
+    private void RemoveTarget(Target target)
+    {
+        if (CurrentTarget == target)
+        {
+            cinemachineTargetGroup.RemoveMember(CurrentTarget.transform);
+            CurrentTarget = null;
+        }
+
+        target.OnDestroyEvent -= RemoveTarget;
+        targets.Remove(target);
     }
 
 }
